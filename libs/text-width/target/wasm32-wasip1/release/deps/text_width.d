fa8990f00123bf7ba5f97a77ded784340153f9d0
renierr <renierr@users.noreply.github.com>
2026-09-04 16:26:01 +0200

    Ignore text-width Cargo build artifacts

--- libs/text-width/target/wasm32-wasip1/release/deps/text_width.d
+++
@@ -1,5 +0,0 @@
-/home/cody/dev/wasm/wasm-demo/libs/text-width/target/wasm32-wasip1/release/deps/text_width.d: src/lib.rs
-
-/home/cody/dev/wasm/wasm-demo/libs/text-width/target/wasm32-wasip1/release/deps/text_width.wasm: src/lib.rs
-
-src/lib.rs:
--- libs/text-width/target/wasm32-wasip1/release/text_width.d
+++
@@ -1,1 +0,0 @@
-/home/cody/dev/wasm/wasm-demo/libs/text-width/target/wasm32-wasip1/release/text_width.wasm: /home/cody/dev/wasm/wasm-demo/libs/text-width/src/lib.rs
